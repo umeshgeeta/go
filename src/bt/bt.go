d0919e0ab5b042aec20905e1b6699c38d07d181a
umeshpatil <umesh409@yahoo.com>
2020-03-21 15:57:20 -0700

    Changed data structure from an array to node pointers since AVL operations will be very expensive otherwise. Added the array representation of the binary tree - bfs - for pyramid printing purposes.

--- src/bt/bt.go
+++ src/bt/bt.go
@@ -5,12 +5,16 @@
 // where root resides is designated as '1', next at '2' and so forth.
 // If height of the binary tree is 'h'; there will be h levels in the tree.
 //
+//
 // It prints the binary tree on console as a pyramid. It computes
 // spaces so that the pyramid is depicted as closely as possible.
 package bt
 
 import (
+	"container/list"
+	"errors"
 	"fmt"
+	"log"
 	"math"
 	"strconv"
 )
@@ -18,10 +22,36 @@ import (
 // *************************************
 // Struct & Constant definitions
 // *************************************
+type Comparable interface {
+	// if 'this' equals c2:	returns 0
+	// if 'this' is greater than c2: returns 1
+	// if 'this' is smaller than c2: returns -1
+	// else error
+	Compare(c2 Comparable) (int, error)
+
+	// returns the length of the value for printing purposes
+	Len() int
+
+	// Returns the string representation of this value
+	Str() string
+}
+
+type NumVal struct {
+	Val int
+}
+
 type Bt struct {
-	Nodes []int
-	Height int
-	MaxVal int			// maximum value encountered so that we know how many digits to provision per node
+	Root *Node
+	NodeCount int
+}
+
+type Node struct {
+	Value Comparable
+	Parent *Node
+	LeftChild *Node
+	RightChild *Node
+	LeftDepth int
+	RightDept int
 }
 
 const TileSize = 4
@@ -31,43 +61,245 @@ const TileChar = " "
 // *************************************
 // Constructor
 // *************************************
-func New(h int) *Bt {
-	if h < 1 {
-		fmt.Printf("Invalid Binary Tree Hieght, minimum value is 1.")
-		return nil
-	}
+func NewBt() *Bt {
 	bt := new(Bt)
-	bt.Height = h
-	// make sure that height is set before invoking nodeCount
-	sz := bt.nodeCount()
-	bt.Nodes = make([]int, sz)
-	bt.MaxVal = math.MinInt64
 	return bt
+}
+
+func NewBtWithRoot(rv Comparable) *Bt {
+	bt := new(Bt)
+	root := Node {rv, nil, nil, nil, 0, 0}
+	bt.SetChild(&root, nil, 0)
+	return bt
+}
+
+func NewBtWithNumValRoot(val int) *Bt {
+	nv := NumVal{val}
+	return NewBtWithRoot(nv)
 }
 
 // *************************************
 // Methods - Exposed
 // *************************************
 
-// Simply inserts the given value at the specified index.
-func (bt* Bt) Insert(index int, value int){
-	if index < 0 || index >= len(bt.Nodes) {
-		fmt.Printf("Index %d is invalid for binary tree with height %d", index, bt.Height)
+// Interface implementation, we introduce a type NumVal as Comparable.
+// Below is the implementation of how that comparision should be done.
+func (nv NumVal) Compare(c2 Comparable) (int, error) {
+	result := 0
+	var err error
+	f, ok := c2.(NumVal)
+	if ok {
+		if nv.Val > f.Val {
+			result = 1
+		} else if nv.Val < f.Val {
+			result = -1
+		}
+		// else existing 0 value is good
+	} else {
+		err = errors.New("Argument Comparable is not of type NumVal")
 	}
-	bt.Nodes[index] = value
-	if bt.MaxVal < value {
-		bt.MaxVal = value
+	return result, err
+}
+
+func (nv NumVal) Len() int {
+	return len(nv.Str())
+}
+
+func (nv NumVal) Str() string {
+	return strconv.Itoa(nv.Val)
+}
+
+// byWhat: It can be +1 or -1 or some other when the entire subtree is appended or chopped off
+// whichChild: 1 for Left Child and 0: for Right Child
+func (node* Node) ChangeDepth(byWhat int, whichChild int) {
+	if whichChild == 1 {
+		node.LeftDepth += byWhat
+		if node.Parent != nil {
+			// recursive call
+			node.Parent.ChangeDepth(byWhat, whichChild)
+		}
+	} else if whichChild == 0 {
+		node.RightDept += byWhat
+		if node.Parent != nil {
+			// recursive call
+			node.Parent.ChangeDepth(byWhat, whichChild)
+		}
+	} else {
+		log.Fatal(fmt.Sprintf("Wrong depth argument %d for increaseDepth call", whichChild))
 	}
+}
+
+// Throw an error if the designated place is already occupied.
+// Argument lr indicates whether to set this node as left child (lr == 0)or
+// right child (lr == 1) of the parent. When parent is nil, we treat
+// invocation of the method as to set the root in which case argument lr is ignored.
+func (bt* Bt) SetChild(node *Node, parent *Node, lr int) error {
+	var err error
+	err = nil
+	// if parent is nil and the tree does not have any root, we can place the node at the root
+	if parent == nil {
+		if bt.Root == nil {
+			bt.Root = node
+			bt.NodeCount = 1
+			node.LeftDepth = 0
+			node.RightDept = 0
+		} else {
+			err = errors.New("Root already exists, cannot set.")
+		}
+	} else {
+		switch lr {
+		case 0:
+			if parent.RightChild == nil {
+				parent.RightChild = node
+				node.Parent = parent
+				bt.NodeCount++
+				node.ChangeDepth(1, 0)
+			} else {
+				err = errors.New("Right child already exists, cannot set.")
+			}
+		case 1:
+			if parent.LeftChild == nil {
+				parent.LeftChild = node
+				node.Parent = parent
+				node.ChangeDepth(1, 1)
+				bt.NodeCount++
+			} else {
+				err = errors.New("Left child already exists, cannot set.")
+			}
+		}
+	}
+	return err
+}
+
+func (bt* Bt) SetNode(index int, node *Node) error {
+	var err error
+	path := GetPath(index)
+	levels := path.Len()
+	if levels > 0 {
+		e := path.Front()
+		path.Remove(e)	// remove the root from the list
+		// we expect the first node to be root and value at 0
+		child := e.Value.(int)
+		if child == 0 {
+			level := 1
+			parent := bt.Root
+			for e = path.Front(); e != nil && err == nil; e = e.Next() {
+				child = e.Value.(int)
+				level++
+				if level < levels {
+					if child == 0 {
+						if parent != nil {
+							// even - it is Right Child
+							parent = parent.RightChild
+						} else {
+							err = errors.New(fmt.Sprintf("No parent node at level %d to attach right child", level))
+						}
+					} else if child == 1 {
+						if parent != nil {
+							// odd - it is Left Child
+							parent = parent.LeftChild
+						} else {
+							err = errors.New(fmt.Sprintf("No parent node at level %d to attach left child", level))
+						}
+					} else {
+						err = errors.New(fmt.Sprintf("Unexpected path value %w", e.Value))
+					}
+				}
+				// else the last level is where the node is to be placed actually
+			}
+			// we set it as the Child
+			bt.SetChild(node, parent, child)
+		} else {
+			err = errors.New("Invalid path, did not find expected root node at the start")
+		}
+	} else {
+		err = errors.New(fmt.Sprintf("No vaiable path available for index %d", index))
+	}
+	return err
+}
+
+func (bt* Bt) Insert(index int, val int) error {
+	n := NumVal{val}
+	nd := Node{
+		Value:      n,
+		Parent:     nil,
+		LeftChild:  nil,
+		RightChild: nil,
+		LeftDepth:  0,
+		RightDept:  0,
+	}
+	return bt.SetNode(index, &nd)
+}
+
+func (bt Bt) Height() int {
+	h := 0
+	if bt.Root != nil {
+		depth := int(math.Max(float64(bt.Root.LeftDepth), float64(bt.Root.RightDept)))
+		h = depth + 1	// we need to add the root
+	}
+	return h
+}
+
+// Returns the tree as a list upon traversing as Breadth First Search (BFS).
+// Where a node is absent, 'nil' is added as the element.
+func (bt Bt) Bfs() (list.List, int) {
+	l := list.New()
+	maxlen := 0
+	if bt.Root != nil {
+		max := bt.MaxNodesPossible()
+		nodesAdded := 0
+		e := l.PushBack(bt.Root)
+		maxlen = bt.Root.Value.Len()
+		nodesAdded++
+		var pn *Node
+		var chl *Node
+		var chr *Node
+		for nodesAdded < max {
+			if e.Value != nil {
+				pn = e.Value.(*Node)
+				if pn != nil {
+					chl = pn.LeftChild
+					chr = pn.RightChild
+					ln := pn.Value.Len()
+					if maxlen < ln {
+						maxlen = ln
+					}
+				} else {
+					chl = &Node{nil, nil,nil,nil,0, 0}
+					chr = &Node{nil, nil,nil,nil,0, 0}
+					l.PushBack(chl)
+					l.PushBack(chr)
+				}
+				l.PushBack(chl)
+				l.PushBack(chr)
+				nodesAdded += 2
+
+			} else {
+				// wrap the nil
+				n := Node{nil, nil,nil,nil,0, 0}
+				//l.InsertAfter(n, e)
+				l.PushBack(&n)
+			}
+			e = e.Next()
+		}
+	}
+	// essentially it is all empty tree, so we return empty list
+	return *l, maxlen
 }
 
 // Returns the pyramid string which can be printed.
 // Gaps are called as tiles. Numbers are padded to maximum value so as alignment works.
 func (bt Bt) Pyramid() string {
 	result := ""
-	tpr := tilesPerRow(bt.Height)
-	for level := 1; level <= bt.Height; level++ {
-		td := tileDist(tpr, level, bt.Height)
-		result = result + bt.fillRow(level, td) + "\n"
+	th := bt.Height()
+	tpr := tilesPerRow(th)
+	bfs, maxl := bt.Bfs()
+	where := bfs.Front()
+	for level := 1; level <= th; level++ {
+		td := tileDist(tpr, level, th)
+		str, wh :=  bt.fillRow(level, td, bfs, where, maxl)
+		result  = result + str + "\n"
+		where = wh
 	}
 	return result
 }
@@ -77,42 +309,80 @@ func (bt Bt) Pyramid() string {
 // *************************************
 
 // Fills the row for the given level
-func (bt Bt) fillRow(level int, td []int) string {
+func (bt Bt) fillRow(level int, td []int, bfs list.List, where *list.Element, maxlen int) (string, *list.Element) {
 	result := ""
 	nodesInLevel := numPerLevel(level)
-	charsToAdjust := (bt.maxNodesInARow() - nodesInLevel) * bt.maxNodeLength()
-	start, _ := indices(level)
+	charsToAdjust := (bt.maxNodesInARow() - nodesInLevel) * maxlen
 	charsPerNode := charsToAdjust / nodesInLevel
 	tdl := len(td)
 	for k := 0; k < tdl-1; k++ {
 		result += tileStr(td[k])
-		result += bt.padNode(start+k, charsPerNode)
+		valstr := "nil"
+		if where != nil {
+			nd := where.Value.(*Node)
+			if nd != nil {
+				ndv := nd.Value
+				if ndv != nil {
+					valstr = ndv.Str()
+				}
+			}
+		}
+		result += bt.padNode(valstr, charsPerNode, maxlen)
+		where = where.Next()
 	}
 	result += tileStr(td[tdl-1])
-	return result
+	return result, where
 }
 
 // Pad the value at a node so as it aligns to the value with maximum digits.
-func (bt Bt) padNode(index int, additional int) string {
-	return pad(bt.Nodes[index], bt.maxNodeLength(), additional)
+func (bt Bt) padNode(val string, additional int, maxlen int) string {
+	return pad(val, maxlen, additional)
 }
 
-func (bt Bt) maxNodeLength() int {
-	return len(strconv.Itoa(bt.MaxVal))
-}
-
-func (bt Bt) nodeCount() int {
-	return int(math.Pow(2, float64(bt.Height))) - 1
+// Note that it is NOT how many'nodes' are present in the tree;
+// but how many can be 'effectively' packed in this binary tree.
+func (bt Bt) MaxNodesPossible() int {
+	return int(math.Pow(2, float64(bt.Height()))) - 1
 }
 
 // Indicates how many node - values - can be maximum in a row,
 // that row is the last row or the last level.
 func (bt Bt) maxNodesInARow() int {
-	return numPerLevel(bt.Height)
+	return numPerLevel(bt.Height())
 }
 
+//func (bt Bt) setChild(parent int, childIndex int, childVal int) bool {
+//	result := false
+//	if childIndex < bt.nodeCount() {
+//		bt.Insert(childIndex,childVal)
+//		result = true
+//	}
+//	return result
+//}
+
 // *************************************
-// Functions
+// Functions - Exposed
+// *************************************
+
+func GetPath(index int) *list.List {
+	l := list.New()
+	i := index
+	for i >= 0 {
+		r := i % 2
+		l.PushFront(r)
+		switch r {
+		case 0:
+			i = i/2 - 1
+		case 1:
+			i = (i-1) / 2
+		}
+	}
+	return l
+}
+
+
+// *************************************
+// Functions - Not Exposed
 // *************************************
 
 // returns number of nodes for a given level
@@ -135,8 +405,7 @@ func indices(l int) (int, int){
 // will have only 4 numbers. So the padding for those 4 additional
 // numbers on the last row needs to be distributed among 4 numbers
 // when we deal with padding of numbers on level 3.
-func pad(num int, ps int, additional int) string {
-	ns := strconv.Itoa(num)
+func pad(ns string, ps int, additional int) string {
 	d := len(ns)
 	result := ""
 	totalChars := additional + ps - d
--- src/main.go
+++ src/main.go
@@ -5,24 +5,27 @@ package main
 import (
 	"./bt"
 	"fmt"
+	"log"
 )
 
 // Tests binary tree pyramid printing.
 func main() {
+	m := make(map[int]int)
+	m[0] = 250
+	m[1] = 134
+	m[3] = 78
+	bt1 := buildBinaryTree(m)
+	fmt.Println(bt1.Pyramid())
+}
 
-	bt2 := bt.New(3)
-	bt2.Insert(0, 25)
-	bt2.Insert(3, 756)
-	bt2.Insert(5, 17)
-	bt2.Insert(6, 52)
-	fmt.Println(bt2.Pyramid())
-
-	bt3 := bt.New(4)
-	bt3.Insert(0, 5)
-	bt3.Insert(3, 76)
-	bt3.Insert(5, 107)
-	bt3.Insert(7, 523)
-	bt3.Insert(11, 24)
-	fmt.Println(bt3.Pyramid())
-
+func buildBinaryTree(val map[int]int) bt.Bt {
+	bt2 := bt.NewBt()
+	for index := range val {
+		fmt.Println(index)
+		e := bt2.Insert(index, val[index])
+		if e != nil {
+			log.Fatal(e)
+		}
+	}
+	return *bt2
 }
