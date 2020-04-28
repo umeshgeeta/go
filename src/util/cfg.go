144b1fc5776855c078b46078739bced28680ff99
umeshpatil <umesh409@yahoo.com>
2020-04-28 03:18:31 -0700

    Adding back lost changes in the previous commit.

--- src/util/cfg.go
+++ src/util/cfg.go
@@ -1,1 +1,25 @@
 package util
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"log"
+)
+
+type ConfigHome struct {
+	Dir     string `env:"GO_CFG_HOME" env-description:"Directory where we can find s3downloader.cfg file"`
+}
+
+var cfgHome ConfigHome
+
+func ReadCfg(cfg interface{}, fileName string) error {
+	err := cleanenv.ReadEnv(&cfgHome)
+	if err != nil {
+		log.Fatal("Error reading environment variable GO_CFG_HOME")
+	}
+	if len(cfgHome.Dir) == 0 {
+		log.Fatal("Environment variable GO_CFG_HOME is not defined")
+	}
+	cfgFileName := cfgHome.Dir + "/" + fileName
+
+	return cleanenv.ReadConfig(cfgFileName, cfg)
+}
