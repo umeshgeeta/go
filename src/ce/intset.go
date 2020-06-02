82a2692db0ec521665b0c83f43cfcee94bd1c78e
umeshpatil <umesh409@yahoo.com>
2020-06-02 08:46:13 -0700

    Changed name to avoid duplicate name issue.

--- src/ce/intset.go
+++ src/ce/intset.go
@@ -4,25 +4,25 @@
  */
 package main
 
-type IntSet struct {
+type IntegerSet struct {
 	intBoolMap map[int]bool
 }
 
-func NewSet() *IntSet {
-	return &IntSet{make(map[int]bool)}
+func NewSet() *IntegerSet {
+	return &IntegerSet{make(map[int]bool)}
 }
 
-func (set *IntSet) Add(i int) bool {
+func (set *IntegerSet) Add(i int) bool {
 	_, found := set.intBoolMap[i]
 	set.intBoolMap[i] = true
 	return !found //False if it existed already
 }
 
-func (set *IntSet) Get(i int) bool {
+func (set *IntegerSet) Get(i int) bool {
 	_, found := set.intBoolMap[i]
 	return found //true if it existed already
 }
 
-func (set *IntSet) Remove(i int) {
+func (set *IntegerSet) Remove(i int) {
 	delete(set.intBoolMap, i)
 }
