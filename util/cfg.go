4792d1f8ff4066a1f20f5602dd4d92b1b592c41f
umeshpatil <umesh409@yahoo.com>
2020-06-07 17:53:09 -0700

    Added support for picking log configurations from any config file which contains a JSON member LogSettings. Added documentation comments and a minor unit test.
    Added a coding exercise to understand how reflection works in Go Lang.

--- util/cfg.go
+++ util/cfg.go
@@ -1,17 +1,29 @@
 // MIT License
 // Author: Umesh Patil, Neosemantix, Inc.
 
-// Package util contains various types and functions used in ancillary mode.
+// Package util contains common functionality which is required by a typical
+// server side any GO application. Namely it contains functionality to read
+// JSON configuration file (from ilyakaznacheev) and rotating logging
+// functionality (from lumberjack). Essentially these are simply few useful
+// wrappers convenient for any GO application.
+//
+// Environment variable GO_CFG_HOME is checked to see if contains provided
+// configuration file. If found, it is read and configuration structure is
+// returned. If this environmental variable is not set, no configuration is
+// read or returned.
 package util
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/ilyakaznacheev/cleanenv"
+	"io/ioutil"
 	"log"
+	"os"
 )
 
 type ConfigHome struct {
-	Dir string `env:"GO_CFG_HOME" env-description:"Directory where we can find s3downloader.cfg file"`
+	Dir string `env:"GO_CFG_HOME" env-description:"Directory where we can find configuration file"`
 }
 
 var cfgHome ConfigHome
@@ -20,10 +32,14 @@ var cfgHome ConfigHome
 func ReadCfg(cfg interface{}, fileName string) error {
 	err := cleanenv.ReadEnv(&cfgHome)
 	if err != nil {
-		log.Fatal("Error reading environment variable GO_CFG_HOME")
+		msg := fmt.Sprintf("Error reading environment variable GO_CFG_HOME: %v", err)
+		fmt.Println(msg)
+		log.Fatal(msg)
 	}
 	if len(cfgHome.Dir) == 0 {
-		log.Fatal("Environment variable GO_CFG_HOME is not defined")
+		msg := fmt.Sprintf("Environment variable GO_CFG_HOME is not defined")
+		fmt.Println(msg)
+		log.Fatal(msg)
 	}
 	cfgFileName := cfgHome.Dir + "/" + fileName
 	fmt.Printf("Config file in use %s\n", cfgFileName)
@@ -34,4 +50,34 @@ func ReadCfg(cfg interface{}, fileName string) error {
 func GetCfgHomeDir() (string, error) {
 	err := cleanenv.ReadEnv(&cfgHome)
 	return cfgHome.Dir, err
+}
+
+// Return the byte array of LogSettings in the passed filename.
+func extractLogSettings(fileName string) (string, error) {
+	err := cleanenv.ReadEnv(&cfgHome)
+	if err != nil {
+		fmt.Println(fmt.Sprintf("Error reading environment variable GO_CFG_HOME: %v", err))
+	}
+	if len(cfgHome.Dir) == 0 {
+		fmt.Println(fmt.Sprintf("Environment variable GO_CFG_HOME is not defined"))
+	}
+	cfgFileName := cfgHome.Dir + "/" + fileName
+	// Open our jsonFile
+	jsonFile, err := os.Open(cfgFileName)
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		return "", err
+	}
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	var result map[string]interface{}
+	json.Unmarshal([]byte(byteValue), &result)
+
+	// get value of log settings
+	rr := result["LogSettings"]
+	fmt.Printf("LogSettings: %v\n", rr)
+	buf, err := json.Marshal(rr)
+	fmt.Println(string(buf))
+	return string(buf), err
 }
