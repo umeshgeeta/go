be420182706d5f7463376f373218a44317d13d6f
umeshpatil <umesh409@yahoo.com>
2023-04-18 19:04:21 -0700

    Merge two sorted arrays - from the other end.

---
+++ src/ce/lce-88-mergesortedarrays.go
@@ -0,0 +1,57 @@
+/*
+ * https://leetcode.com/problems/merge-sorted-array/description/
+ *
+ * You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n,
+ * representing the number of elements in nums1 and nums2 respectively.
+ *
+ * Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+ *
+ * The final sorted array should not be returned by the function, but instead be stored inside the array nums1.
+ * To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged,
+ * and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+ *
+ * Copyright (c) 2023. Neosemantix, Inc.
+ * Author: Umesh Patil
+ */
+
+package main
+
+import "fmt"
+
+// The trick is start from the other end - star placing largest element at the other end i.e. on right hand side and
+// start from there towards the smallest element. That way no moves are wasted. And of course handle boundry conditions.
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	i := m - 1
+	j := n - 1
+	k := m + n - 1
+	for k > -1 {
+		if i < 0 {
+			nums1[k] = nums2[j]
+			j--
+		} else if j < 0 {
+			nums1[k] = nums1[i]
+			i--
+		} else {
+			if nums1[i] > nums2[j] {
+				nums1[k] = nums1[i]
+				i--
+			} else {
+				nums1[k] = nums2[j]
+				j--
+			}
+		}
+		k--
+	}
+}
+
+func main() {
+	nums1 := []int{1, 2, 3, 0, 0, 0}
+	nums2 := []int{2, 5, 6}
+	merge(nums1, 3, nums2, 3)
+	fmt.Println(nums1)
+
+	nums1 = []int{0}
+	nums2 = []int{1}
+	merge(nums1, 0, nums2, 1)
+	fmt.Println(nums1)
+}
